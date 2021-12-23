54f22ed183ec69ecc4916ba5c1b23706ecff36e5
hnt-neptunium <94439219+hnt-neptunium@users.noreply.github.com>
2021-12-23 11:54:14 -0600

    Update fxmanifest.lua

--- hnt-weapons/fxmanifest.lua
+++ hnt-weapons/fxmanifest.lua
@@ -2,5 +2,5 @@ version '1.0.0'
 author 'hnt-neptunium'
 description 'Default-type weapons pack for FiveM servers. Edited with love by Neptunium for Hybrid Networking.'
 
-fx_version 'bodacious'
+fx_version 'cerulean'
 game 'gta5'
